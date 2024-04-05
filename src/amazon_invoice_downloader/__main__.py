dc4eb8a9aa80f360f433ae89608e712f6fc2c321
Luke Burnett <46521994+luke-burnett@users.noreply.github.com>
2024-04-05 20:02:59 +0100

    Delete src/amazon_invoice_downloader directory

--- src/amazon_invoice_downloader/__main__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present David C Wang <dcwangmit01@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-import sys
-
-if __name__ == "__main__":
-    from amazon_invoice_downloader.cli import amazon_invoice_downloader
-
-    sys.exit(amazon_invoice_downloader())
